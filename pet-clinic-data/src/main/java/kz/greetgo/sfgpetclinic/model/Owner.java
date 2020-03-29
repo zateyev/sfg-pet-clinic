9d4f6b905b9df2ba5c70e5c6e7f266a69154a7dd
zhateyev <zhateyev@greet-go.com>
2020-03-29 19:58:08 +0600

    Added tests. Fix OwnerBuilder. Closes #49

--- pet-clinic-data/src/main/java/kz/greetgo/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/kz/greetgo/sfgpetclinic/model/Owner.java
@@ -1,6 +1,9 @@
 package kz.greetgo.sfgpetclinic.model;
 
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.HashSet;
@@ -9,11 +12,18 @@ import java.util.Set;
 @Setter
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "owners")
 public class Owner extends Person {
+
+  @Builder
+  public Owner(Long id, String firstName, String lastName, String address, String city, String telephone, Set<Pet> pets) {
+    super(id, firstName, lastName);
+    this.address = address;
+    this.city = city;
+    this.telephone = telephone;
+    this.pets = pets;
+  }
 
   @Column(name = "address")
   private String address;
