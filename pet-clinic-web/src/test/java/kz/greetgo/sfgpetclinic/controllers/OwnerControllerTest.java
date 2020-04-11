3e4ddd9c17da62cb63dfad1127bff302756f587c
zhateyev <zhateyev@greet-go.com>
2020-04-11 09:41:35 +0600

    Fix display Owner. Closes #62

--- pet-clinic-web/src/test/java/kz/greetgo/sfgpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/kz/greetgo/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -14,7 +14,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -50,8 +51,8 @@ class OwnerControllerTest {
 
     mockMvc.perform(get("/owners"))
       .andExpect(status().isOk())
-    .andExpect(view().name("owners/index"))
-    .andExpect(model().attribute("owners", hasSize(2)));
+      .andExpect(view().name("owners/index"))
+      .andExpect(model().attribute("owners", hasSize(2)));
   }
 
   @Test
@@ -71,5 +72,15 @@ class OwnerControllerTest {
       .andExpect(view().name("notimplemented"));
 
     verifyZeroInteractions(ownerService);
+  }
+
+  @Test
+  void displayOwner() throws Exception {
+    when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
+
+    mockMvc.perform(get("/owners/123"))
+      .andExpect(status().isOk())
+      .andExpect(view().name("owners/ownerDetails"))
+      .andExpect(model().attribute("owner", hasProperty("id", is(1L))));
   }
 }
