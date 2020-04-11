89b34293560956988fcdc59eca5c90aa5eab9725
zhateyev <zhateyev@greet-go.com>
2020-04-11 10:33:25 +0600

    Fix find by like. Closes #63

--- pet-clinic-web/src/main/java/kz/greetgo/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/kz/greetgo/sfgpetclinic/controllers/OwnerController.java
@@ -43,18 +43,16 @@ public class OwnerController {
     }
 
     // find owners by last name
-    List<Owner> results = ownerService.findAllByLastNameLike(owner.getLastName());
+    List<Owner> results = ownerService.findAllByLastNameLike("%" + owner.getLastName() + "%");
     if (results.isEmpty()) {
       // no owners found
       result.rejectValue("lastName", "notFound", "not found");
       return "owners/findOwners";
-    }
-    else if (results.size() == 1) {
+    } else if (results.size() == 1) {
       // 1 owner found
       owner = results.get(0);
       return "redirect:/owners/" + owner.getId();
-    }
-    else {
+    } else {
       // multiple owners found
       model.addAttribute("selections", results);
       return "owners/ownersList";
