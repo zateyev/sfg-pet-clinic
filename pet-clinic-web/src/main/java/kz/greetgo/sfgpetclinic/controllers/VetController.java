7053fb79b0f5403c6c84f328bd77ddb9336438ab
zhateyev <zhateyev@greet-go.com>
2020-04-20 21:32:45 +0600

    Implement Vets JSON Endpoint. Closes #69

--- pet-clinic-web/src/main/java/kz/greetgo/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/kz/greetgo/sfgpetclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package kz.greetgo.sfgpetclinic.controllers;
 
+import kz.greetgo.sfgpetclinic.model.Vet;
 import kz.greetgo.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -18,6 +23,12 @@ public class VetController {
   public String listVets(Model model) {
     model.addAttribute("vets", vetService.findAll());
     return "vets/index";
+  }
+
+  @GetMapping("/api/vets")
+  public @ResponseBody Set<Vet> getVetsJson() {
+
+    return vetService.findAll();
   }
 
 }
